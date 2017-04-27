adf21b0552eabe14eb2f8a89d89182305cca5b0b
dhoni <dhoni.p.saputra@gmail.com>
2017-04-27 21:05:04 +0700

    major update

--- js/Modules/module.js
+++ js/Modules/module.js
@@ -13,7 +13,7 @@ window.mainApp
 })
 .run(['$rootScope', function($rootScope) {
     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
-        $rootScope.title = current.$$route.title;
+        $rootScope.title = current.$$route.title? current.$$route.title : '';
     });
 }])
 .factory('global_configuration', function(){
@@ -28,32 +28,6 @@ window.mainApp
 window.mainApp.run(['$rootScope', '$location', '$routeParams','$config', '$owner', '$authorize', function ($rootScope, $location, $routeParams, $config, $owner, $authorize) {
     $rootScope.$on('$routeChangeStart', function (event, next, current) {
 
-    	/*var Cook = window.sessionStorage.getItem('auth');
-        var source = $config.source;
-        if (source == 'panel' && !Cook) {
-            event.preventDefault();
-            $location.path('/login');
-            $rootScope.is_auth = 0;
-            $rootScope.style_main_panel= 'width:100%';
-            $rootScope.sidebar= false
-        }else
-        {
-            if(next.params.owner_key)
-            {
-                $rootScope._owner_key = next.params.owner_key;
-
-            }else
-            {
-                $rootScope._owner_key = '';
-            }
-            $config.set_public_key($rootScope._owner_key);
-
-            // console.log($config, $rootScope._owner_key, next.params);
-            $rootScope.is_auth = 1;
-            $rootScope.sidebar= true
-            $rootScope.style_main_panel= '';
-            $rootScope.base_url = $config.base_url;
-        }*/
         $rootScope.is_auth = 0;
         $rootScope.style_main_panel= 'width:100%';
         $rootScope.sidebar= false
